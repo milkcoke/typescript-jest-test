c90e3469dcdf4f25c091c992ec6ba6fa72aa34bd
milkcoke <mbh023@gmail.com>
2023-06-02 23:41:02 +0900

    test: Apply `concurrent` on calculator unit test

--- test/unit-test/domain/calculator.test.ts
+++ test/unit-test/domain/calculator.test.ts
@@ -3,7 +3,7 @@ import {Calculator} from '../../../src/domain/calculator'
 describe('Calculator', ()=>{
   const calculator = new Calculator()
 
-  test('add - should yield 0 if an empty array is provided', ()=>{
+  test.concurrent('add - should yield 0 if an empty array is provided', ()=>{
     // Arrange
     const nums = []
     // Act
@@ -12,7 +12,7 @@ describe('Calculator', ()=>{
     expect(res).toEqual(0)
   })
 
-  test('fail - transformed to number when input is number string', ()=>{
+  test.concurrent('fail - transformed to number when input is number string', ()=>{
     const value = '45'
 
     const res = calculator.transformToNumber(value)
@@ -20,7 +20,7 @@ describe('Calculator', ()=>{
     expect(typeof res).toBe('number')
   })
 
-  test('NaN is number type', ()=>{
+  test.concurrent('NaN is number type', ()=>{
     const infinityNumber = 1 / 0
     expect(infinityNumber).not.toBeNaN()
     expect(typeof infinityNumber).toEqual('number')
